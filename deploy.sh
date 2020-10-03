d3ecf01f8bec5063c588c8e08bf987d6da1fdfc1
Alexander <pankinsasha24@gmail.com>
2020-10-03 22:36:15 +0700

    Add deployment

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ set -e
 IMAGE="${DOCKER_USERNAME}/${DOCKER_REPOSITORY}"
 GIT_VERSION=$(git describe --always --abbrev --tags --long)
 
-yarn build
-
 docker build --file Dockerfile.deploy -t "${IMAGE}":"${GIT_VERSION}" .
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
@@ -13,9 +11,9 @@ docker push "${IMAGE}":"${GIT_VERSION}"
 
 chmod 600 deploy_rsa
 
-ssh -i deploy_rsa "${SERVER_USER}"@"${SERVER_IP}" <<"EOF"
+ssh -i deploy_rsa "${SERVER_USER}"@"${SERVER_IP}" <<EOF
   docker stop ${DOCKER_REPOSITORY}
   docker container rm ${DOCKER_REPOSITORY}
-  docker run --restart unless-stopped -d -p 9002:9000 --name ${DOCKER_REPOSITORY} ${IMAGE}:${GIT_VERSION}
+  docker run --restart unless-stopped -d -p ${PORT_ON_HOST}:9000 --name ${DOCKER_REPOSITORY} ${IMAGE}:${GIT_VERSION}
   docker system prune -f -a
 EOF
